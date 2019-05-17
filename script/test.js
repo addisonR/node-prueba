79cbc2ff65135cbbdb597c81c286aa861d10cc84
addison <addison.rivero@gmail.com>
2019-05-17 10:43:41 -0400

    dockerfile

--- script/test.js
+++ script/test.js
@@ -1,8 +1,10 @@
-var assert = require('chai').assert;
+var chai = require('chai');
+var assert = chai.assert;
 var app = require('../app.js');
  
 describe('App', function() {
-  it('app should return hello world', function() {
+  it('app should return hello world', function(done) {
     assert.equal(app(),'hello world');
+	done();
   });
 });
